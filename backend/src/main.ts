248dffb4384a1734c68a2412f5ad9996739e34f6
William Dromard <william.dromard@epitech.eu>
2025-04-21 03:05:32 +0900

    back: take out the assets path => GCP

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -20,25 +20,6 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Accept', 'ngrok-skip-browser-warning'],
     credentials: true, // Optional, if you use cookies or auth
   });
-
-  // Serve static assets with custom CORS headers
-  app.useStaticAssets(join(__dirname, '..', 'Uploads'), {
-    prefix: '/uploads/',
-    setHeaders: (res) => {
-      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-      res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
-      res.setHeader(
-        'Access-Control-Allow-Headers',
-        'ngrok-skip-browser-warning',
-      );
-      // Set permissive CSP to avoid image loading issues
-      res.setHeader(
-        'Content-Security-Policy',
-        "default-src 'self'; img-src 'self' https://*.ngrok-free.app data:;",
-      );
-    },
-  });
-
   await app.listen(3000);
 }
 bootstrap();
